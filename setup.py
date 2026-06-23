e678263066660490f7deb2776a24369bc649933c
vishalshelke97 <vshelke989@gmail.com>
2026-06-23 18:35:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirments
+    '''
+    requirments=[]
+    with open(file_path)as file_obj:
+        requirments=file_obj.readline()
+        requirments=[req.replace("\n","") for req in requirments]
+
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+    return requirments
+
+
+setup(
+    name='mlprojects',
+    version='0.0.1',
+    author='vishal',
+    author_email='vshelke989@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments('requirments.txt')
+
+)
